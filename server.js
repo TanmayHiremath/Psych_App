29119f07a3adc8b8aecf3e1204c3358ea0922bec
Tanmay Hiremath <tanmay.hiremath@gmail.com>
2020-07-25 22:10:06 +0530

    third commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const socketIO = require('socket.io');
 
 const PORT = process.env.PORT || 3000;
-const INDEX = '/index.html';
+const INDEX = '/home.html';
 
 const server = express()
   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
