1d6dad48d85cbe1070c66c43f88f72e98fbb724b
Tanmay Hiremath <tanmay.hiremath@gmail.com>
2020-07-25 23:53:32 +0530

    third commi

--- server.js
+++ server.js
@@ -2,15 +2,15 @@ const express = require('express');
 const socketIO = require('socket.io');
 
 const PORT = process.env.PORT || 3000;
-const INDEX = '/home.html';
+
 
 const server = express()
-  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 const io = socketIO(server);
 
 io.on('connection', (socket) => {
+  io.emit('connected','Date:'+ new Date().toTimeString())
   console.log('Client connected');
   socket.on('disconnect', () => console.log('Client disconnected'));
 });
